bcb2455fa3614306c1b28aeff8aaaedfcc23ed3b
rioto.nagano <kingdom950927@gmail.com>
2022-09-25 15:06:23 +0900

    propsで数値の受け渡し

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Article from "./components/Article"
 function App() {
   return (
     <div>
-      <Article title={'新日本一わかりやすいReact入門'} content={'今日のコンテンツはpropsについて。'}/>
+      <Article
+      title={'新日本一わかりやすいReact入門'}
+      content={'今日のコンテンツはpropsについて。'}
+      order={3}
+      />
     </div>
   );
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,6 +3,7 @@ const Article = (props) => {
     <div>
       <h2>{props.title}</h2>
       <p>{props.content}</p>
+      <p>{props.order}</p>
     </div>
   )
 }
