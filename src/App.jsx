b8e4fd4a1108309348b9bd544af76095fefb8335
rioto.nagano <kingdom950927@gmail.com>
2022-09-25 15:03:55 +0900

    AppコンポーネントからArticleコンポーネントへpropsの受け渡し

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Article from "./components/Article"
+
+
+function App() {
+  return (
+    <div>
+      <Article title={'新日本一わかりやすいReact入門'} content={'今日のコンテンツはpropsについて。'}/>
+    </div>
+  );
+}
+
+export default App;
