f24db9845e6dec2e74afe481bfcca3dc03007e21
rioto.nagano <kingdom950927@gmail.com>
2022-09-25 15:37:30 +0900

    filter,map関数とkeyを用いた特定の条件下で繰り返し処理の作成

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Article from "./components/Article"
 function App() {
   const authorName = 'トラハック'
 
-  const arr = ["りんご","みかん","バナナ","キウイ"]
+  const arr = ["りんご","みかん","バナナ","キウイ","パイナップル","グレープフルーツ"]
 
   return (
     <div>
@@ -16,8 +16,13 @@ function App() {
       isPublished={true}
       authorName={authorName}
       />
+      <p>map関数を用いた繰り返し処理</p>
       <ul>
         {arr.map((fruit, i) => <li key={i}>{fruit}</li>)}
+      </ul>
+      <p>map,filter関数を用いた特定の条件下での繰り返し処理</p>
+      <ul>
+        {arr.filter(fruit => fruit.includes("ル")).map((fruit, i) => <li key={i}>{fruit}</li>)}
       </ul>
     </div>
   );
