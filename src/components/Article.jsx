825ab5e29f85da20922899f0a71ce1e4ee62dc0a
rioto.nagano <kingdom950927@gmail.com>
2022-09-25 16:24:00 +0900

    useStateを使用した公開ボタンの中身の切り替え

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const Article = (props) => {
+  const [ispublished, setIsPublished] = useState(false);
+  console.log(ispublished);
+
   return (
     <div>
       <h2>{props.title}</h2>
       <p>{props.content}</p>
       <p>{props.authorName}</p>
+      <button onClick={() => setIsPublished(true)}>公開</button>
     </div>
   )
 }
