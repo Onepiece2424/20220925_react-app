6aa3e79e05faddcea53218f80e3c4591a43cca2d
rioto.nagano <kingdom950927@gmail.com>
2022-09-25 19:16:03 +0900

    useEffect、fetchを使用しuseEffect内でAPIを呼び出す。

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import Article from "./components/Article"
 // import TextInput from "./components/TextInput"
-import Counter from "./components/Counter"
+// import Counter from "./components/Counter"
 import ToggleButton from "./components/ToggleButton"
 
 function App() {
   // const authorName = 'トラハック'
-
   // const arr = ["りんご","みかん","バナナ","キウイ","パイナップル","グレープフルーツ"]
+
+  const [name, setName] = useState()
+
+  const [id, setId] = useState('deatiger')
+
+  const ids = ['deatiger', 'gaearon', 'aws', 'google', 'facebook', 'Onepiece2424']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setName(data.name)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }, [id])
 
   return (
     <div>
+      <p>{id}の、github上の名前は{name}です。</p>
+      <button onClick={getRandomId}>IDを変更</button>
+      <br></br>
+      <br></br>
       <ToggleButton />
       <br></br>
       <br></br>
